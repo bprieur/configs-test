0ab14e9a79f898d5a17efb719bf7a31488ad1e63
Baptiste PRIEUR <bprieur@enssat.fr>
2021-01-20 01:04:34 +0100

    short config test

---
+++ librairy.py
@@ -0,0 +1,21 @@
+class BaseConfig:
+    DEBUG=False
+    TESTING=False
+    HOST="127.0.0.1"
+    PORT=5000
+
+class DevConfig(BaseConfig):
+    DEBUG=True
+    PORT=5002
+    
+    @property
+    def HOST(self):
+        return "0.0.0.0"
+    
+
+class TestConfig(BaseConfig):
+    TESTING=True
+
+
+class ProdConfig(BaseConfig):
+    PORT=8082
